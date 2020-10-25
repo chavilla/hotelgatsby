ea0f17dbfaf1a3d6521250788e5eec0723e51d72
chavilla <jcharris.villa@gmail.com>
2020-10-25 15:12:52 -0500

    Se configura y empiezan a aplicar css

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React from "react"
+import { Global, css } from '@emotion/core';
+import Header from "./header"
+import Helmet from "react-helmet"
 
-const Layout = () => {
-    return ( 
-        <h1>Desde Layout</h1>
-     );
+const Layout = props => {
+  return (
+    <>
+        <Global
+            styles={css`
+                html{
+                    font-size:62.5%;
+                }
+                body{
+                    background-color:#e1e1e1;
+                    font-size:16px;
+                    font-size:1.6rem;
+                    line-height:1.5;
+                }
+                h1,h2,h3{
+                    margin:0;
+                    line-height:1.5;
+                }
+                h1,h2{
+                    font-family:'Roboto', sans-serif
+                }
+                h3{
+                    font-family: 'PT Sans', sans-serif;
+                }
+                ul{
+                    list-style:none;
+                    margin:0;
+                    padding:0;
+                }
+            `}
+        />
+      <Helmet>
+        <title>Gatsby Hotel</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"/>
+        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet"></link>
+      </Helmet>
+      <Header />
+      {props.children}
+    </>
+  )
 }
- 
-export default Layout;
+
+export default Layout
